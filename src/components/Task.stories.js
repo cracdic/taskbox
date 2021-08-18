d66b22b793a458b0bf8bca6ab8b33836d893ac08
lolo <lolo@learning.com>
2021-08-18 23:32:01 +0800

    first commit

---
+++ src/components/Task.stories.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Tasky } from './Task';
+
+
+export default {
+    component: Tasky,
+    title: 'Task',
+};
+
+
+const Template = args => <Tasky {...args} />;
+
+
+export const Default = Template.bind({});
+Default.args = {
+    task: {
+        id: '1',
+        title: 'Test Task',
+        state: 'TASK_INBOX',
+        updatedAt: new Date(2018, 0, 1, 9, 0),
+    },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args.task,
+    state: 'TASK_ARCHIVED',
+  },
+};
